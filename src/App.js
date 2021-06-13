b28473f1f10427fefbaedaebb324742d2129c5ac
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-06-13 23:54:46 +0900

    import transaction&list components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { Transaction } from './components/Transaction';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
+        <Transaction />
       </div>
     </div>
   );
