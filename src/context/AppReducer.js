afa68d236d3fdeb25a1099ec68ba804abb3b0d17
JiyeonMoon <jiyeonmoon814@gmail.com>
2021-06-14 00:12:00 +0900

    create globalState&appReducer

---
+++ src/context/AppReducer.js
@@ -0,0 +1,9 @@
+export default (state, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
+
+//how we specify the application state changes 
+//in response to certain actions to our store, context
